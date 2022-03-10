f43381fffb3b122a1c3ef77b411923e9b2fe1d15
BrunoLofranoPorto <89394173+BrunoLofranoPorto@users.noreply.github.com>
2022-03-10 15:48:00 -0300

    Add files via upload

---
+++ src/views/ArtigosPeriodicos.vue
@@ -0,0 +1,17 @@
+<template>
+  <iframe src="https://brunolofranoporto.github.io/lagas-webgis-artigos/" frameborder="0" height="900px" width="100%"></iframe>
+</template>
+
+<script>
+import { collapsed, toggleSidebar } from '@/components/sidebar/state'
+
+export default {
+  setup() {
+    return { collapsed, toggleSidebar }
+  }
+}
+</script>
+
+<style scoped>
+    
+</style>
